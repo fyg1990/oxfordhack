2f6936b032d8c1ed9e674c8500334669a2303ffb
fangyuan.guan <guanfangyuan@gmail.com>
2018-11-25 04:16:45 +0000

    add ui asset

--- application.py
+++ application.py
@@ -1,9 +1,21 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, send_from_directory
 import api
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='')
 
 @app.route("/get-all-records")
 def records():
     all_record = api.get_all_record()
     return jsonify(all_record)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
